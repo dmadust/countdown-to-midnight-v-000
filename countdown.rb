59dfaafabd261a446fd78107ce7c23a54b49370f
dmadust@gmail.com <dmadust@gmail.com>
2018-05-16 06:59:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,10 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
+  while seconds > 0
+    sleep(1)
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
